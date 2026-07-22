0e904de002e9d4bb6d30b6a183f1889dfa16ff3a
Fauzan-F <fauzanfirdaus1966@gmail.com>
2026-07-22 13:46:47 +0700

    commit jawa ku

--- src/pages/KulinerPage.tsx
+++ src/pages/KulinerPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 import { useScrollReveal } from '../hooks/useScrollReveal';
